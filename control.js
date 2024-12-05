34efc977e3c00a912286a054bc9684e7365e823d
A7mdooh <80976049+A7mdooh@users.noreply.github.com>
2024-12-05 17:53:06 +0400

    Update control.js

--- control.js
+++ control.js
@@ -1,12 +1,12 @@
 // إعداد Firebase
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBGv3JmZB71ztx-PwR_wMmT0f8jZlDDT2U",
+    authDomain: "ai-voice-interface.firebaseapp.com",
+    databaseURL: "https://ai-voice-interface-default-rtdb.firebaseio.com",
+    projectId: "ai-voice-interface",
+    storageBucket: "ai-voice-interface.firebasestorage.app",
+    messagingSenderId: "997645849088",
+    appId: "1:997645849088:web:ffc8183b9a0015adb1bee5"
 };
 
 // تهيئة Firebase
